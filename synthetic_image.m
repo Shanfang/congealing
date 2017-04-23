8cc08ee5b37e312548cf8bc5842b0ada23817717
Shanfang <shanfangzhao@gmail.com>
2017-04-23 15:31:39 -0400

    Generate two synthetic images

---
+++ synthetic_image.m
@@ -0,0 +1,19 @@
+%% Gray image
+rows = 256;
+columns = 256;
+grayImage = ones(rows, columns, 'uint8').*200;
+xCoords= [80 180 180 80 80];
+yCoords = [80 80 180 180 80];
+mask = poly2mask(xCoords, yCoords, rows, columns);
+grayImage(mask) = 80; 
+%% First component Gray+noise
+im_normal=double(grayImage./max(grayImage(:)));
+im_noise= imnoise(im_normal,'gaussian',0,0.02);
+%% Second component: Inhomogeneous term
+X = 1:rows;                           % X is a vector from 1 to imageSize
+X0 = (X / rows) -0.2;                 % rescale X 
+Xm = meshgrid(X0, X0);             % 2D matrices
+%% Output image
+Out_Img=im_noise.*Xm;
+subplot(121);imshow(grayImage);
+subplot(122);imshow(Out_Img,[]);
